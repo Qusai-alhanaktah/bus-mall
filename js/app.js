bddacf20be7a653b1d582984627f790b371a551f
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-11-25 14:27:18 +0200

    finshed

--- js/app.js
+++ js/app.js
@@ -19,11 +19,9 @@ var names = [
     "usb",
     "water-can",
     "wine-glass",
-]
-var voting = 0;
-var time = 0;
+];
+
 var counter = 0;
-var votes = [];
 var left = document.querySelector("#left");
 var middle = document.querySelector("#middle");
 var right = document.querySelector("#right");
@@ -34,13 +32,13 @@ var showing = document.querySelector("#showing");
 function Sales(name) {
     this.name = name;
     this.imagePath = `img/${name}.jpg`;
+    this.voting = 0;
+    this.views = 0;
     Sales.all.push(this);
 }
 Sales.all = [];
 for (var i = 0; i < names.length; i++) {
     new Sales(names[i]);
-
-
 }
 
 
@@ -48,7 +46,12 @@ function render() {
     var leftSales = Sales.all[randomNumber(0, Sales.all.length - 13)];
     var middleSales = Sales.all[randomNumber(Sales.all.length - 12, Sales.all.length - 6)];
     var rightSales = Sales.all[randomNumber(Sales.all.length - 5, Sales.all.length - 1)];
-    // while((leftSales !== middleSales) &&(leftSales !== rightSales )) { 
+    // while (leftSales == middleSales) {
+        
+    // }
+    leftSales.views++;
+    middleSales.views++;
+    rightSales.views++;
     left.setAttribute("src", leftSales.imagePath);
     left.setAttribute("alt", leftSales.name);
     left.setAttribute("title", leftSales.name);
@@ -59,40 +62,51 @@ function render() {
     right.setAttribute("alt", rightSales.name);
     right.setAttribute("title", rightSales.name);
 
-    // }
+
+
 }
 render();
 
 
 
-
-
-imageSection.addEventListener("click", function (e) {
+function handleClick(e) {
 
     counter++;
-    if (counter < 26) {
-        if (event.target.id !== "imageSection") {
-            render();
-           
-                    votes[counter]=(left, voting++);
-               
+    if (counter < 25) {
+        if (e.target.id !== "imageSection") {
+            for (let x = 0; x < Sales.all.length; x++) {
+                if (e.target.title === Sales.all[x].name) {
+                    Sales.all[x].voting++;
+                }
             }
+            console.table(Sales.all);
+            render();
         }
-        
-        
-        
-        console.log(votes);
-    });
 
+    }
+   
+    if (counter == 25){
+         showwww();
+     }
+    }
 
-
-
-
-
+imageSection.addEventListener("click", handleClick);
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
+function showwww(){
+    var container = document.getElementById("showing");
+    var ulEl = document.createElement("ul");
+    container.appendChild(ulEl);
+    console.log(container)
+    for (let z = 0; z < Sales.all.length; z++) {
 
+        var liEl = document.createElement("li");
+        ulEl.appendChild(liEl);
+        liEl.textContent = `${Sales.all[z].name} Slicer had ${Sales.all[z].voting} votes and was shown ${Sales.all[z].views} times`;
+        console.table(Sales.all[z]);
+    }
+}
